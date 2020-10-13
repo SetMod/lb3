c72e24f55e2a9b1133e0cc02d697fcc538121a90
Andre Filipe Georgiyovich <voroncka.andrey@gmail.com>
2020-10-13 12:36:39 +0300

    com2

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,38 +1,39 @@
-var mongoose = require('mongoose'); 
-var log = require('./log')(module); 
-mongoose.connect('mongodb://localhost/test1'); 
-var db = mongoose.connection; 
-var config = require('./config'); 
+var mongoose = require('mongoose');
+var log = require('./log')(module);
+mongoose.connect('mongodb://localhost/test1');
+var db = mongoose.connection;
+var config = require('./config');
 mongoose.connect(config.get('mongoose:uri'));
 
-db.on('error', function (err) { 
-	log.error('connection error:', err.message); 
-}); 
-
-db.once('open', function callback () { 
-	log.info("Connected to DB!"); 
-}); 
-
-var Schema = mongoose.Schema; // Schemas 
-var Images = new Schema({ 
-	kind: { 
-		type: String, 
-		enum: ['thumbnail', 'detail'], 
-	required: true }, 
-url: { type: String, required: true } 
+db.on('error', function (err) {
+	log.error('connection error:', err.message);
 });
 
-var Article = new Schema({ 
-	 title: { type: String, required: true }, 
-	 author: { type: String, required: true }, 
-	 description: { type: String, required: true }, 
-	 images: [Images],
-	 modified: { type: Date, default: Date.now } 
+db.once('open', function callback() {
+	log.info("Connected to DB!");
+});
+
+var Schema = mongoose.Schema; // Schemas 
+var Images = new Schema({
+	kind: {
+		type: String,
+		enum: ['thumbnail', 'detail'],
+		required: true
+	},
+	url: { type: String, required: true }
+});
+
+var Article = new Schema({
+	title: { type: String, required: true },
+	author: { type: String, required: true },
+	description: { type: String, required: true },
+	images: [Images],
+	modified: { type: Date, default: Date.now }
 }); // validation 
 
-Article.path('title').validate(function (v) { 
-	return v.length > 5 && v.length < 70; 
-}); 
+Article.path('title').validate(function (v) {
+	return v.length > 5 && v.length < 70;
+});
 
-var ArticleModel = mongoose.model('Article', Article); 
+var ArticleModel = mongoose.model('Article', Article);
 module.exports.ArticleModel = ArticleModel;
